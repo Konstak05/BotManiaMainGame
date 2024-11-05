a7fce13e2b378b28683d28c2fe2bd3dc4369c858
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-11-05 19:42:09 +0200

    new enemy level meter stat

--- Assets/EnemyCounter.cs
+++ Assets/EnemyCounter.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class EnemyCounter : MonoBehaviour
 {
-
     public TextMeshPro Test;
-
+    public Slider BounceMul;
 
     void Update()
     {
         Test.text = GlobalData.GetEnemyCount().ToString();
+        BounceMul.value = GlobalData.GetEnemyCount();
     }
 }
