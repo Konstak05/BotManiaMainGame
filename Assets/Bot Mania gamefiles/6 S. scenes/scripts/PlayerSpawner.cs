15a7796c4cb238efb442c27834887d70abdfda26
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-11-12 20:40:28 +0200

    Major commit moment

--- Assets/Bot Mania gamefiles/6 S. scenes/scripts/PlayerSpawner.cs
+++ Assets/Bot Mania gamefiles/6 S. scenes/scripts/PlayerSpawner.cs
@@ -8,15 +8,9 @@ public class PlayerSpawner : MonoBehaviour
 
     public string Location;
     public string Goal;
-    public bool IsCreativeMode;
-    public bool IsTutorialMode;
-    public bool IsFromTeleporter;
-    public bool IsIndoors;
-    public bool IsStoryMode;
+    public bool IsCreativeMode,IsTutorialMode,IsFromTeleporter,IsIndoors,IsStoryMode;
 
-    public AudioClip TeleportSound1;
-    public AudioClip TeleportSound2;
-    public AudioClip TeleportSound3;
+    public AudioClip TeleportSound1,TeleportSound2,TeleportSound3;
     public AudioSource Sound;
     public Vector3 SpawningPos;
     public Quaternion Rotation;
@@ -24,98 +18,57 @@ public class PlayerSpawner : MonoBehaviour
     public GameObject Minimap;
     public Gunscript UnlockObject2;
 
-    public AudioSource Ambient;
-    public AudioSource Combat;
+    public AudioSource Ambient,Combat;
+    public AudioClip AmbientStartSoundtrack,CombatStartSoundtrack;
     private float IsAmbient,Isfighting;
 
     void Awake()
     {
         SceneManager.LoadScene("Player", LoadSceneMode.Additive);
-        IsAmbient = 0f;
-        Isfighting = 0f;
+        IsAmbient = 0f; Isfighting = 0f;
 
         GlobalData.Initialize();
-
     }
 
     void Start(){
-    
+
     Minimap = GameObject.Find("Minimap101");
-    if(IsIndoors){
-        Minimap.SetActive(false);
-    }
-    else{
-        Minimap.SetActive(true);
-    }
-    
+    if(IsIndoors){Minimap.SetActive(false);}
+    else{Minimap.SetActive(true);}
+
+    if(IsTutorialMode){UnlockObject2 = GameObject.Find("CameraArea223").GetComponent<Gunscript>(); UnlockObject2.IsTutorialMode2 = true;}
+
     if(IsFromTeleporter){
-    ParticleTeleporter = GameObject.Find("Player boom particle");
-    int RandomTeleport = UnityEngine.Random.Range(0, 3);
+        ParticleTeleporter = GameObject.Find("Player boom particle");
+        int RandomTeleport = UnityEngine.Random.Range(0, 3);
 
-    if(RandomTeleport == 0){
-    float audioVolume = PlayerPrefs.GetFloat("AudioVolume");
-    float masterVolume = PlayerPrefs.GetFloat("MasterVolume");
-    Sound.volume = audioVolume * masterVolume;
-    Sound.PlayOneShot(TeleportSound1);
-    }
-    else if (RandomTeleport == 1){
-    float audioVolume = PlayerPrefs.GetFloat("AudioVolume");
-    float masterVolume = PlayerPrefs.GetFloat("MasterVolume");
-    Sound.volume = audioVolume * masterVolume;
-    Sound.PlayOneShot(TeleportSound2);
-
-    }
-    else if (RandomTeleport == 2){
-    float audioVolume = PlayerPrefs.GetFloat("AudioVolume");
-    float masterVolume = PlayerPrefs.GetFloat("MasterVolume");
-    Sound.volume = audioVolume * masterVolume;
-    Sound.PlayOneShot(TeleportSound3);
-
-    }
-    
-    ParticleTeleporter.SetActive(true);
-
+        if(RandomTeleport == 0){SoundVolumeUpdater1();Sound.PlayOneShot(TeleportSound1);}
+        else if (RandomTeleport == 1){SoundVolumeUpdater1(); Sound.PlayOneShot(TeleportSound2);}
+        else if (RandomTeleport == 2){SoundVolumeUpdater1(); Sound.PlayOneShot(TeleportSound3);}
+        ParticleTeleporter.SetActive(true);
     }
 
-    if(IsTutorialMode){
-    UnlockObject2 = GameObject.Find("CameraArea223").GetComponent<Gunscript>();
-    UnlockObject2.IsTutorialMode2 = true;
-    }
-
+    Ambient.clip = AmbientStartSoundtrack; Combat.clip = CombatStartSoundtrack;
+    Ambient.Play(); Combat.Play();
     }
 
     void Update(){
-
-       if(GlobalData.GetEnemyCount() < 1 && Ambient != null && Combat != null){
-
-        float MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
-        float MasterVolume = PlayerPrefs.GetFloat("MasterVolume", 1.0f);
+        if(GlobalData.GetEnemyCount() < 1 && Ambient != null && Combat != null){
+            //SoundSystem - Ambience
+            SoundVolumeUpdater2();
+            if(IsAmbient < 1){IsAmbient = IsAmbient + 0.01f;} if(Isfighting > 0){Isfighting = Isfighting - 0.01f;}
+        }
+        else if (GlobalData.GetEnemyCount() > 0 && Ambient != null && Combat != null){
+            //SoundSystem - Combat
+            SoundVolumeUpdater2();
+            if(IsAmbient > 0){IsAmbient = IsAmbient - 0.01f;} if(Isfighting < 1){Isfighting = Isfighting + 0.01f;}
+        }
+    }
+    void SoundVolumeUpdater1(){float audioVolume = PlayerPrefs.GetFloat("AudioVolume"); float masterVolume = PlayerPrefs.GetFloat("MasterVolume"); 
+        Sound.volume = audioVolume * masterVolume;   
+    }
+    void SoundVolumeUpdater2(){float MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f); float MasterVolume = PlayerPrefs.GetFloat("MasterVolume", 1.0f);
         Ambient.volume = MusicVolume * MasterVolume * IsAmbient;
-        Combat.volume = MusicVolume * MasterVolume * Isfighting;
-
-        if(IsAmbient < 1){
-            IsAmbient = IsAmbient + 0.01f;
-        }
-        if(Isfighting > 0){
-            Isfighting = Isfighting - 0.01f;
-        }
-
-       }
-       else if (GlobalData.GetEnemyCount() > 0 && Ambient != null && Combat != null){
-
-        float MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
-        float MasterVolume = PlayerPrefs.GetFloat("MasterVolume", 1.0f);
-        Ambient.volume = MusicVolume * MasterVolume * IsAmbient;
-        Combat.volume = MusicVolume * MasterVolume * Isfighting;
-
-        if(IsAmbient > 0){
-            IsAmbient = IsAmbient - 0.01f;
-        }
-        if(Isfighting < 1){
-            Isfighting = Isfighting + 0.01f;
-        }
-
-       }
-
+        Combat.volume = MusicVolume * MasterVolume * Isfighting;     
     }
 }
