2dd95345e4010a7cb9d536cbb69db59e5721ee57
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2024-11-21 23:04:07 +0200

    Up-to-date github source code for 1.6.6

--- Assets/Bot Mania gamefiles/2 PressStartMenu/ButtonForMainMenu.cs
+++ Assets/Bot Mania gamefiles/2 PressStartMenu/ButtonForMainMenu.cs
@@ -11,46 +11,28 @@ public class ButtonForMainMenu : MonoBehaviour
     public bool CanPress;
 
     void Start(){
-    Application.targetFrameRate = 60;
-    Invoke("CanPress2", 1);
+        Application.targetFrameRate = 60;
+        Invoke("CanPress2", 1);
     }
 
 
-    void CanPress2()
-    {
-    CanPress = true;
-    }
+    void CanPress2(){CanPress = true;}
 
 
     void FixedUpdate()
     {
-    if (Input.GetKeyDown(KeyCode.Return) && HasStarted == 0 && CanPress && PlayerPrefs.GetInt("FirstTime") == 1)
-    {
-    transition.SetTrigger("Start"); 
-    Invoke("StartGame2", 1);
-    HasStarted = 1;
-    }       
+        if (Input.GetKeyDown(KeyCode.Return) && HasStarted == 0 && CanPress && PlayerPrefs.GetInt("FirstTime") == 1){
+            transition.SetTrigger("Start"); 
+            Invoke("StartGame2", 1);
+            HasStarted = 1;
+        }       
     }
 
-    void StartGame2()
-    {
-    Cursor.lockState = CursorLockMode.None;
-    SceneManager.LoadScene(SceneName);
+    void StartGame2(){Cursor.lockState = CursorLockMode.None; SceneManager.LoadScene(SceneName);}
 
-    }
+    public void OpenDiscordLink(){Application.OpenURL("https://discord.gg/2c82552hZU");}
 
-    public void OpenDiscordLink()
-    {
-    Application.OpenURL("https://discord.gg/2c82552hZU");
-    }
+    public void OpenYoutubeLink(){Application.OpenURL("https://www.youtube.com/channel/UC5oykP-e-wX6H-MMIQbHvyA");}
 
-    public void OpenYoutubeLink()
-    {
-    Application.OpenURL("https://www.youtube.com/channel/UC5oykP-e-wX6H-MMIQbHvyA");
-    }
-
-    public void OpenTwitterLink()
-    {
-    Application.OpenURL("https://twitter.com/konstak05");
-    }
+    public void OpenTwitterLink(){Application.OpenURL("https://twitter.com/konstak05");}
 }
