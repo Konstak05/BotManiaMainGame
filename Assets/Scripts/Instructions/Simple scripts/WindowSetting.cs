ad954bae28d8b09706bb934cf3a007a917fc78b0
overmet15 <metsocool675@gmail.com>
2024-12-01 15:59:32 +0900

    Setting Menu W.I.P

--- Assets/Scripts/Instructions/Simple scripts/WindowSetting.cs
+++ Assets/Scripts/Instructions/Simple scripts/WindowSetting.cs
@@ -2,97 +2,113 @@ using UnityEngine;
 
 public class WindowSetting : MonoBehaviour
 {
-   public GameObject Selected1;
-   public GameObject Selected2;
+  public GameObject Selected1;
+  public GameObject Selected2;
 
 
 
-   void Start(){
-   if(PlayerPrefs.GetInt("Windowed") == 1){
-     Selected1.SetActive(true);
-     Selected2.SetActive(false);
+  void Start()
+  {
+    if (PlayerPrefs.GetInt("Windowed") == 1)
+    {
+      Selected1.SetActive(true);
+      Selected2.SetActive(false);
+    }
+    else
+    {
+      Selected1.SetActive(false);
+      Selected2.SetActive(true);
+    }
+  }
 
-   }
-   else{
-     Selected1.SetActive(false);
-     Selected2.SetActive(true);
-   }
-   }
+  public void ScaleTo(int num)
+  {
+    switch (num)
+    {
+      case 0: ScaleTo0(); break;
+      case 1: ScaleTo1(); break;
+      case 2: ScaleTo2(); break;
+      case 3: ScaleTo3(); break;
+      case 4: ScaleTo4(); break;
+      case 5: ScaleTo5(); break;
+      case 6: ScaleTo6(); break;
+    }
+  }
 
-
-   public void ScaleTo1()
-   {
+  void ScaleTo0()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",2f);
-     Screen.SetResolution(960, 540, windowed);
+    PlayerPrefs.SetFloat("Window", 2f);
+    Screen.SetResolution(960, 540, windowed);
   }
-   public void ScaleTo2()
-   {
+  void ScaleTo1()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",1f);
-     Screen.SetResolution(1280, 720, windowed);
+    PlayerPrefs.SetFloat("Window", 1f);
+    Screen.SetResolution(1280, 720, windowed);
   }
-   public void ScaleTo3()
-   {
+  void ScaleTo2()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",0f);
-     Screen.SetResolution(1920 , 1080, windowed);
+    PlayerPrefs.SetFloat("Window", 0f);
+    Screen.SetResolution(1920, 1080, windowed);
   }
-   public void ScaleTo4()
-   {
+  void ScaleTo3()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",3f);
-     Screen.SetResolution(400 , 225, windowed);
+    PlayerPrefs.SetFloat("Window", 3f);
+    Screen.SetResolution(400, 225, windowed);
   }
-   public void ScaleTo5()
-   {
+  void ScaleTo4()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",4f);
-     Screen.SetResolution(640, 360 , windowed);
+    PlayerPrefs.SetFloat("Window", 4f);
+    Screen.SetResolution(640, 360, windowed);
   }
-   public void ScaleTo6()
-   {
+  void ScaleTo5()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",5f);
-     Screen.SetResolution(800, 450, windowed);
+    PlayerPrefs.SetFloat("Window", 5f);
+    Screen.SetResolution(800, 450, windowed);
   }
-   public void ScaleTo7()
-   {
+  void ScaleTo6()
+  {
     bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-     PlayerPrefs.SetFloat("Window",6f);
-     Screen.SetResolution(1024, 576, windowed);
+    PlayerPrefs.SetFloat("Window", 6f);
+    Screen.SetResolution(1024, 576, windowed);
   }
 
-   public void fullScreen()
-   {
-     PlayerPrefs.SetInt("Windowed",0);
-     Screen.fullScreen = true;
+  public void fullScreen()
+  {
+    PlayerPrefs.SetInt("Windowed", 0);
+    Screen.fullScreen = true;
   }
-   public void windowed()
-   {
-     PlayerPrefs.SetInt("Windowed",1);
-     Screen.fullScreen = false;
+  public void windowed()
+  {
+    PlayerPrefs.SetInt("Windowed", 1);
+    Screen.fullScreen = false;
   }
 
 
-  void Update(){
-   
-   if(PlayerPrefs.GetInt("Windowed") == 1){
-     Selected1.SetActive(false);
-     Selected2.SetActive(true);
+  void Update()
+  {
+    if (PlayerPrefs.GetInt("Windowed") == 1)
+    {
+      Selected1.SetActive(false);
+      Selected2.SetActive(true);
 
-   }
-   else{
-     Selected1.SetActive(true);
-     Selected2.SetActive(false);
-   }
-
+    }
+    else
+    {
+      Selected1.SetActive(true);
+      Selected2.SetActive(false);
+    }
   }
 }
