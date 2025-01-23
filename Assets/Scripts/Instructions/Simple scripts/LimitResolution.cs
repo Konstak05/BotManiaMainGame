d6acba9e1cbf6bd4b23965cb6682d05d7ff50cba
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-23 21:56:07 +0200

    Revert "Bro 😭"
    This reverts commit 7a54866afdcf8e00f404e3444205f27c29c8f44e.

--- Assets/Scripts/Instructions/Simple scripts/LimitResolution.cs
+++ Assets/Scripts/Instructions/Simple scripts/LimitResolution.cs
@@ -6,18 +6,33 @@ public class LimitResolution : MonoBehaviour
     {
         bool windowed = PlayerPrefs.GetInt("Windowed") == 0;
 
-        float windowMode = PlayerPrefs.GetFloat("Window");
-
-        switch (windowMode)
+        if (PlayerPrefs.GetFloat("Window") == 2)
         {
-            case 0: Screen.SetResolution(1920, 1080, windowed); break;
-            case 1: Screen.SetResolution(1280, 720, windowed); break;
-            case 2: Screen.SetResolution(960, 540, windowed); break;
-            case 3: Screen.SetResolution(400, 225, windowed); break;
-            case 4: Screen.SetResolution(640, 360, windowed); break;
-            case 5: Screen.SetResolution(800, 450, windowed); break;
-            case 6: Screen.SetResolution(640, 360, windowed); break;
-            default: Screen.SetResolution(1920, 1080, windowed); break;
+            Screen.SetResolution(960, 540, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 1)
+        {
+            Screen.SetResolution(1280, 720, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 0)
+        {
+            Screen.SetResolution(1920, 1080, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 3)
+        {
+            Screen.SetResolution(400, 225, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 4)
+        {
+            Screen.SetResolution(640, 360, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 5)
+        {
+            Screen.SetResolution(800, 450, windowed);
+        }
+        else if (PlayerPrefs.GetFloat("Window") == 6)
+        {
+            Screen.SetResolution(1024, 576, windowed);
         }
     }
 }
