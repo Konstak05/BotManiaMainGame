5011170ea0d9b019a894020c250c1ef51d54ecce
DESKTOP-20JI703\zkons <zkonst05@gmail.com>
2025-01-23 21:55:21 +0200

    Revert "SceneLoader and stuff"
    This reverts commit 616ac8114a763b49a57142acecf5304a87db11b9.

--- Assets/Scripts/Watermark/WaterMarkStart.cs
+++ Assets/Scripts/Watermark/WaterMarkStart.cs
@@ -1,4 +1,3 @@
-using BotMania;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,29 +7,37 @@ using UnityEngine.SceneManagement;
 public class WaterMarkStart : MonoBehaviour
 {
     public Animator transition;
+    public string SceneName;
     public string SceneName2;
     
     void Start()
     {
-        if(PlayerPrefs.GetFloat("AudioVolume", 0) == 0 && PlayerPrefs.GetFloat("MusicVolume") == 0 && PlayerPrefs.GetFloat("MasterVolume") == 0)
-        {
-            PlayerPrefs.SetFloat("AudioVolume", 1);
-            PlayerPrefs.SetFloat("MusicVolume", 1);
-            PlayerPrefs.SetFloat("MasterVolume", 1);
-        }
-        Invoke("StartGame",7.5f);
+     if(PlayerPrefs.GetFloat("AudioVolume", 0) == 0 && PlayerPrefs.GetFloat("MusicVolume") == 0 && PlayerPrefs.GetFloat("MasterVolume") == 0){
+    PlayerPrefs.SetFloat("AudioVolume", 1);
+    PlayerPrefs.SetFloat("MusicVolume", 1);
+    PlayerPrefs.SetFloat("MasterVolume", 1);
+     }
+
+
+     Invoke("StartGame",7.5f);
     }
 
     // Update is called once per frame
     void StartGame()
     {
-        transition.SetTrigger("Start"); 
-        Invoke("StartGame2", 1);
+    transition.SetTrigger("Start"); 
+    Invoke("StartGame2", 1);
+        
     }
 
     void StartGame2()
     {
-        if(PlayerPrefs.GetInt("FirstTime") == 1) SceneLoader.LoadMenu(SceneLoader.Menus.PressStart);
-        else SceneLoader.LoadScene(SceneName2);
+    if(PlayerPrefs.GetInt("FirstTime") == 1){
+    SceneManager.LoadScene(SceneName);
+    }
+    else{
+    SceneManager.LoadScene(SceneName2);
+    }
+
     }
 }
--- Assets/Scripts/tutorialFinishTeleporter.cs
+++ Assets/Scripts/tutorialFinishTeleporter.cs
@@ -1,36 +1,41 @@
-using BotMania.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace BotMania
+public class tutorialFinishTeleporter : MonoBehaviour
 {
-    public class tutorialFinishTeleporter : MonoBehaviour
+    public Animator transition;
+    private bool HasTeleported = false;
+    public string SceneName;
+
+    void Start(){
+        transition = GameObject.Find("Fading loader2").GetComponent<Animator>();
+
+    }
+
+    void OnTriggerEnter(Collider other)
     {
-        private bool HasTeleported = false;
-        public string SceneName;
-
-        void OnTriggerEnter(Collider other)
+        if (other.CompareTag("Player") && !HasTeleported)
         {
-            if (other.CompareTag("Player") && !HasTeleported)
-            {
 
-                HasTeleported = true;
-                Invoke("StartGame", 0f);
-            }
+            HasTeleported = true;
+            Invoke("StartGame",0f);
         }
+    }
 
-        void StartGame()
-        {
-            PlayerPrefs.SetInt("FirstTime", 1);
-            Invoke("StartGame2", 4);
-        }
+    void StartGame()
+    {
+    transition.SetTrigger("Start"); 
+    PlayerPrefs.SetInt("FirstTime",1);
+    Invoke("StartGame2", 4);
+        
+    }
 
-        void StartGame2()
-        {
-            Cursor.lockState = CursorLockMode.None;
-            SceneLoader.LoadMenu(SceneLoader.Menus.PressStart, 1);
-        }
+    void StartGame2()
+    {
+    Cursor.lockState = CursorLockMode.None;
+    SceneManager.LoadScene(SceneName);
+
     }
 }
